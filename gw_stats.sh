bcc1d39572864b9952b93e3303114b85dc5770f6
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2024-07-18 17:10:51 +0530

    add portforwarding

--- gw_stats.sh
+++ gw_stats.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-#port-forward choreo connect gw before running this script
-# URL to check
-URL="http://localhost:9090/stats"
+NAMESPACE="default"  # Set the Kubernetes namespace
+POD_NAME="your-pod-name"  # Set your pod name
+LOCAL_PORT=9090
+REMOTE_PORT=9090
+URL="http://localhost:${LOCAL_PORT}/stat"
+
+# Function to set up port forwarding
+setup_port_forward() {
+  echo "Setting up port forwarding from ${LOCAL_PORT} to ${REMOTE_PORT} on pod ${POD_NAME}..."
+  kubectl port-forward -n ${NAMESPACE} pod/${POD_NAME} ${LOCAL_PORT}:${REMOTE_PORT} &
+  PORT_FORWARD_PID=$!
+}
+
+# Function to clean up port forwarding
+cleanup_port_forward() {
+  echo "Cleaning up port forwarding..."
+  kill ${PORT_FORWARD_PID}
+}
+
+# Function to check for upstream_cx_overflow, upstream_cx_active, or upstream_cx_total
+check_cx_metrics() {
+  curl -s $URL | grep -E "upstream_cx_overflow|upstream_cx_active|upstream_cx_total"
+}
+
+# Set up port forwarding
+setup_port_forward
 
 # Function to check for cx-overflow
 check_cx_metrics() {
@@ -15,3 +38,6 @@ while true; do
   # Wait for 5 minutes
   sleep 300
 done
+
+# Trap EXIT signal to clean up port forwarding
+trap cleanup_port_forward EXIT
